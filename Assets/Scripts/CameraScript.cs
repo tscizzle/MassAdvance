fc97b7381a62354ce3f2bc4cd86e3b36acab47c8
Tyler S-C <tscizzle@gmail.com>
2020-07-03 20:55:26 -0400

    scroll with mouse scroll instead of moving mouse toward edge

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    private float mouseScrollGain = 100f; // increases overall scroll speed
-    private float mouseScrollDampening = 10f; // lessens the increase in scroll speed as the mouse approaches the edge
-    private float mouseScrollEdgeSize = 0.1f; // portion of screen height
+    private float mouseScrollGain = 30f;
     private float scrollZUpperLimit = -3;
     private float scrollZLowerLimit = -6;
 
@@ -23,29 +21,17 @@ public class CameraScript : MonoBehaviour
     /* HELPERS */
 
     private void mouseScroll()
-    /* Detect that the mouse is near an edge and scroll the camera that direction. */
     {
-        float upperEdgeBoundary = Screen.height * (1 - mouseScrollEdgeSize);
-        float lowerEdgeBoundary = Screen.height * mouseScrollEdgeSize;
+        float scrollInput = Input.mouseScrollDelta.y;
 
-        float moveAmount = 0;
-        if (Input.mousePosition.y > upperEdgeBoundary)
-        {
-            float distFromEdge = Mathf.Abs(Screen.height - Input.mousePosition.y);
-            moveAmount = 1 / (distFromEdge + mouseScrollDampening);
-        } else if (Input.mousePosition.y < lowerEdgeBoundary)
-        {
-            float distFromEdge = Mathf.Abs(Input.mousePosition.y);
-            moveAmount = -1 / (distFromEdge + mouseScrollDampening);
-        }
-
-        if (moveAmount != 0)
+        if (scrollInput != 0)
         {
             Vector3 currentPos = transform.position;
-            float moveDist = moveAmount * mouseScrollGain * Time.deltaTime;
-            float newZ = currentPos.z + moveDist;
+
+            float newZ = currentPos.z + (scrollInput * mouseScrollGain * Time.deltaTime);
             newZ = Mathf.Min(newZ, scrollZUpperLimit);
             newZ = Mathf.Max(newZ, scrollZLowerLimit);
+            
             transform.position = new Vector3(currentPos.x, currentPos.y, newZ);
         }
     }
