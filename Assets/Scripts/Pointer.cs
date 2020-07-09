9921db1ab6b90bbc9fdc17e303fcb540d125cfdc
Tyler S-C <tscizzle@gmail.com>
2020-07-08 22:20:07 -0400

    Pointer shows over removed blocks too

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /* PUBLIC API */
+
+    public static IEnumerator displayPointer(Vector2 gridIndices)
+    {
+        GameObject pointerObj = PrefabInstantiator.P.CreatePointer(gridIndices);
+        yield return new WaitForSeconds(GameLogic.G.secondsBetweenActions);
+        Destroy(pointerObj);
+    }
+}
